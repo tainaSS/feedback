66b5f2f7a2747cabc3981ce03b83f16bc2197eee
tainaSS Santos <taina.santoss@eaportal.org>
2024-06-09 23:50:08 +0000

    style: posicionamento no cen do cardtro da tela

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,5 @@
 export const theme = {
-    homeBackground: "#1315118",
+    homeBackground: "#131518",
     cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
     textColor: "#969FAD",
     submitButtonColor: "#FC7614",
