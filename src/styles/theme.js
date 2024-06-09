05f10a50290b3bba960b29772689197203d62c80
tainaSS Santos <taina.santoss@eaportal.org>
2024-06-09 22:39:36 +0000

    feat: instalação e implantação do react-router-dom e do styled-components

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    homeBackground: "#1315118",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
+    textColor: "#969FAD",
+    submitButtonColor: "#FC7614",
+    white: "#fff",
+    feedbackButtonColor: "#262E38",
+    feedbackButtonText: "#7C8798"
+}
